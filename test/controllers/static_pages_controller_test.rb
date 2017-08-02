2d90ec1284fce6bbc95a52b0d91eb1d2b32dcfda
lachlan <lachlan.miller.1990@outlook.com>
2017-08-03 00:28:01 +0900

    First

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,18 @@
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "| 東京コーヒー・カフェー"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home #{@base_title}"
+  end
+
+  test "should get home" do
+    get home_path
+    assert_response :success
+    assert_select "title", "Home #{@base_title}"
+  end
+end
