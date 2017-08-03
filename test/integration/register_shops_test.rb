4c094518b012da97597a59516796bd424e89e8a7
lachlan <lachlan.miller.1990@outlook.com>
2017-08-03 20:58:05 +0900

    user can sign in in test

--- test/integration/register_shops_test.rb
+++ test/integration/register_shops_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class RegisterShopsTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @password = "password"
+    @confirmed_user = User.create(email: "#{rand(4000)}@example.com", 
+                                  password: @password)
+  end
+
+  test "successful login of confirmed user" do
+    sign_in(user: @confirmed_user, password: @password)
+    assert_redirected_to root_url
+  end
+
+
+
   test 'invalid shop registration' do
     get new_shop_path
     assert_no_difference 'Shop.count' do
@@ -12,6 +26,10 @@ class RegisterShopsTest < ActionDispatch::IntegrationTest
     end
     assert_template 'shops/new'
     assert_select ".error.message", count: 1
+  end
+
+  test 'require user login to register shop' do
+
   end
 
   test 'valid shop registration' do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,5 +5,12 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def sign_in(user:, password:)
+    post user_session_path, params: {
+      user: {
+        email: user.email,
+        password: password
+      }
+    }
+  end
 end
