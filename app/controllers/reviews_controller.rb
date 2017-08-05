45f4eda7c7763893a595594d4c5519f23051a28d
lachlan <lachlan.miller.1990@outlook.com>
2017-08-05 11:46:11 +0900

    user can create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,34 @@
 class ReviewsController < ApplicationController
+	before_action :authenticate_user!, except: [:show, :index]
+
 	def index
 	end
 	
 	def create
-		@review = Review.find(params[:id])
+		@shop = Shop.find(params[:shop_id])
+		@review = @shop.reviews.build(review_params)
+		@review.user = current_user
+
+		if (@review.save)
+			flash[:success] = 'Review created'
+			redirect_to shop_review_path(@shop, @review)
+		else
+			render 'new'
+		end
 	end
 
 	def new
 		@shop = Shop.find(params[:shop_id])
-		puts @shop.reviews
 		@review = @shop.reviews.build
+	end
+
+	def show
+		@review = Review.find(params[:id])	
+	end
+
+	private
+
+	def review_params
+		params.require(:review).permit(:id, :comment, shop_attributes: [:shop_id])
 	end
 end
---
+++ test/integration/user_creates_a_review_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class UserCreatesAReviewTest < ActionDispatch::IntegrationTest
+	def setup
+    @shop = shops(:shop_one)
+    @district = districts(:district_one)
+    @password = "password"
+    @user = User.create(email: "#{rand(4000)}@example.com", 
+                                  password: @password)
+	end
+
+	test "user logs in and creates a review" do
+    sign_in(user: @user, password: @password)
+		get new_shop_review_path(@shop)
+		assert_template 'reviews/new'
+		
+		assert_difference 'Review.count', 1 do
+		post shop_reviews_path, params: {
+			shop: @shop,
+			review: {
+				comment: 'Some comment'
+			}
+		}
+		end
+	end
+
+	test "non authenticated user fails to create a review" do
+		get new_shop_review_path(@shop)
+
+		assert_no_difference 'Review.count' do
+			post shop_reviews_path, params: {
+				shop: @shop,
+				review: {
+					comment: 'Some comment'
+				}
+			}
+		end
+	end
+
+	test "users fills in a form to create a review" do
+    sign_in(user: @user, password: @password)
+		# visit new_shop_review_path(@shop)
+		# assert_template 'reviews/new'
+	end
+end
